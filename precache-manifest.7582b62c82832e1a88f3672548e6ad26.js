cc318db59f1537579f8c495fe4344dcae988e63a
Shadocks404 <61094328+Shadocks404@users.noreply.github.com>
2020-07-01 18:59:46 +0200

    Add files via upload

---
+++ precache-manifest.7582b62c82832e1a88f3672548e6ad26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26bccc6e8dee74c943e4f6032690e2fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70b07775a2f73a443078",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "75dbe1a15b6b559d5458",
+    "url": "./static/css/main.f7e6ce70.chunk.css"
+  },
+  {
+    "revision": "70b07775a2f73a443078",
+    "url": "./static/js/2.1e301aa8.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "./static/js/2.1e301aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dbe1a15b6b559d5458",
+    "url": "./static/js/main.3cd31405.chunk.js"
+  },
+  {
+    "revision": "c1d61fb98cef66711430",
+    "url": "./static/js/runtime-main.9d8c414c.js"
+  }
+]);
